240bfabbb14d3ec31a739f51bb9a60590fcdb118
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-12-03 18:38:31 +0200

    add day 03 part 2 solution

--- 2022/03/solution.py
+++ 2022/03/solution.py
@@ -12,8 +12,36 @@ f = open("input.txt", "r")
 # Uppercase item types A through Z have priorities 27 through 52
 
 
-def find_common_elem(str1: str, str2: str):
-    arr = set(str1).intersection(str2)
+# def find_common_elem(str1: str, str2: str):
+#     arr = set(str1).intersection(str2)
+#     return arr.pop()
+
+
+# def get_priority(c: str):
+#     if c.islower():
+#         return ord(c) - ord('a') + 1
+#     else:
+#         return ord(c) - ord('A') + 1 + 26
+
+
+# lines = [l.removesuffix("\n") for l in f.readlines()]
+
+# sum_priorities = 0
+
+# for l in lines:
+#     half = len(l) // 2
+#     common_elem = find_common_elem(l[0:half], l[half:len(l)])
+#     sum_priorities += get_priority(common_elem)
+
+# print('Result:', sum_priorities)
+
+
+#
+# Part 2
+#
+
+def find_common_elem(str1: str, str2: str, str3: str):
+    arr = set(str1) & set(str2) & set(str3)
     return arr.pop()
 
 
@@ -28,9 +56,8 @@ lines = [l.removesuffix("\n") for l in f.readlines()]
 
 sum_priorities = 0
 
-for l in lines:
-    half = len(l) // 2
-    common_elem = find_common_elem(l[0:half], l[half:len(l)])
+for i in range(0, len(lines), 3):
+    common_elem = find_common_elem(lines[i], lines[i+1], lines[i+2])
     sum_priorities += get_priority(common_elem)
 
 print('Result:', sum_priorities)
