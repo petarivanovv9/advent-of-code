84bf94cf61e16766f7d500d7b9ff8b11247c6209
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-12-04 13:58:03 +0200

    add day 04 part 2 solution

--- 2022/04/solution.py
+++ 2022/04/solution.py
@@ -2,6 +2,36 @@ f = open("input.txt", "r")
 
 lines = [l.removesuffix("\n") for l in f.readlines()]
 
+
+#
+# Part 1
+#
+
+# counter = 0
+
+# for l in lines:
+#     # [['13-53'], ['17-82']]
+#     raw_pairs = list(map(lambda x: x.split('-'), l.split(',')))
+#     pairs = [
+#         [int(raw_pairs[0][0]), int(raw_pairs[0][1])],
+#         [int(raw_pairs[1][0]), int(raw_pairs[1][1])]
+#     ]
+
+#     if set(range(pairs[0][0], pairs[0][1] + 1)).issubset(range(pairs[1][0], pairs[1][1] + 1)):
+#         counter += 1
+#         continue
+
+#     if set(range(pairs[1][0], pairs[1][1] + 1)).issubset(range(pairs[0][0], pairs[0][1] + 1)):
+#         counter += 1
+#         continue
+
+# print('Result:', counter)
+
+
+#
+# Part 2
+#
+
 counter = 0
 
 for l in lines:
@@ -12,11 +42,11 @@ for l in lines:
         [int(raw_pairs[1][0]), int(raw_pairs[1][1])]
     ]
 
-    if set(range(pairs[0][0], pairs[0][1] + 1)).issubset(range(pairs[1][0], pairs[1][1] + 1)):
+    if set(range(pairs[0][0], pairs[0][1] + 1)) & set(range(pairs[1][0], pairs[1][1] + 1)):
         counter += 1
         continue
 
-    if set(range(pairs[1][0], pairs[1][1] + 1)).issubset(range(pairs[0][0], pairs[0][1] + 1)):
+    if set(range(pairs[1][0], pairs[1][1] + 1)) & set(range(pairs[0][0], pairs[0][1] + 1)):
         counter += 1
         continue
 
