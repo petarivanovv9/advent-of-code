e3f0f1604e01666f9adbfd16673994eefcc050b2
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-12-07 10:54:01 +0200

    add day 06 part 1 solution

---
+++ 2022/06/solution.py
@@ -0,0 +1,16 @@
+f = open("input.txt", "r")
+
+lines = [l.removesuffix("\n") for l in f.readlines()]
+
+line = lines[0]
+
+len_marker = 4
+
+res = -1
+
+for i in range(0, len(line) - len_marker):
+    if len(set(line[i:i + len_marker])) == len_marker:
+        res = i + len_marker
+        break
+
+print('Res:', res)
