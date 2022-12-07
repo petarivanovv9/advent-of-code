e10d45b6a0a4a5cdfea9d42d5953414ff72f4861
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-12-07 11:03:01 +0200

    add day 06 part 2 solution

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -4,7 +4,26 @@ lines = [l.removesuffix("\n") for l in f.readlines()]
 
 line = lines[0]
 
-len_marker = 4
+#
+# Part 1
+#
+
+# len_marker = 4
+
+# res = -1
+
+# for i in range(0, len(line) - len_marker):
+#     if len(set(line[i:i + len_marker])) == len_marker:
+#         res = i + len_marker
+#         break
+
+# print('Res:', res)
+
+#
+# Part 2
+#
+
+len_marker = 14
 
 res = -1
 
