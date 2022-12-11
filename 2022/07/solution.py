d5ede29395cdb299c8d80badf519bcc4763bf591
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-12-11 17:11:01 +0200

    add day 07 part 1 solution

---
+++ 2022/07/solution.py
@@ -0,0 +1,34 @@
+import os
+from collections import defaultdict
+from pathlib import Path
+# from pprint import pprint
+
+# f = open("input.txt", "r")
+f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
+
+lines = [l.removesuffix("\n") for l in f.readlines()]
+
+curr_dir = Path("/")
+
+dir_sizes = defaultdict(int)
+
+for l in lines:
+    if l.startswith("$ cd"):
+        _, __, subpath = l.split(" ")
+        curr_dir = (curr_dir / subpath).resolve()
+    elif l.startswith("$ ls"):
+        continue
+    elif l.startswith("dir"):
+        continue
+    else:
+        f_size_raw, _ = l.split(" ")
+        f_size = int(f_size_raw)
+
+        dir_sizes[curr_dir] += f_size
+
+        for p in curr_dir.parents:
+            dir_sizes[p] += f_size
+
+res = sum(v for v in dir_sizes.values() if v <= 100000)
+
+print('Res:', res)
