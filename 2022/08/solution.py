018c602e4c8da1fc02df8a23dbe0765db7fa47a2
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-12-15 19:24:55 +0200

    add day 08 part 2 solution

--- 2022/08/solution.py
+++ 2022/08/solution.py
@@ -36,3 +36,50 @@ for i in range(m):  # rows
                 res += 1
 
 print('Res:', res)
+
+
+#
+# Part 2
+#
+
+m = len(matrix)  # height
+n = len(matrix[0])  # width
+
+max_score = 0
+
+for i in range(m):  # rows
+    for j in range(n):  # columns
+        score = 1
+
+        tree_score = 0
+        for x in range(j-1, -1, -1):  # left
+            tree_score += 1
+            if matrix[i][x] >= matrix[i][j]:
+                break
+        score *= tree_score
+
+        tree_score = 0
+        for x in range(j+1, n):  # right
+            tree_score += 1
+            if matrix[i][x] >= matrix[i][j]:
+                break
+        score *= tree_score
+
+        tree_score = 0
+        for y in range(i-1, -1, -1):  # up
+            tree_score += 1
+            if matrix[y][j] >= matrix[i][j]:
+                break
+        score *= tree_score
+
+        tree_score = 0
+        for y in range(i+1, m):  # down
+            tree_score += 1
+            if matrix[y][j] >= matrix[i][j]:
+                break
+        score *= tree_score
+
+        max_score = max(max_score, score)
+
+
+print('Res:', max_score)
